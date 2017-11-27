82d1cc8b9a8fb1f4f6bee235fd571a9c4dd13b9b
Jeppe <Jeppe@82.211.194.74>
2017-11-27 18:28:49 +0100

    Initial.

--- src/juniorMatador/Run.java
+++ src/juniorMatador/Run.java
@@ -1,10 +1,9 @@
 package juniorMatador;
 
 public class Run {
-
+	
     public static void main(String[] args) {
-        Game juniorMatador = new Game();
-
+    	Game juniorMatador = new Game();
         juniorMatador.playGame();
     }
 }
